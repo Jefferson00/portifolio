d9abe432b15e8d34880b5e8617bc8642ff3d2bf7
Jefferson00 <jeffersonts00@gmail.com>
2021-04-14 23:45:02 -0300

    links added

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -6,19 +6,19 @@ export function Socials(){
         <div className={styles.socialsContainer}>
             <p>Entre em contato</p>
             <div className={styles.icons}>
-                <a href="#">
+                <a href="https://github.com/Jefferson00"  target="_blank" rel="noopener noreferrer">
                     <span className={styles.iconGithub}></span>
                 </a>
-                <a href="#">
+                <a href="mailto:jeffersonts00@gmail.com.br"  target="_blank" rel="noopener noreferrer">
                     <span className={styles.iconGmail}></span>
                 </a>
-                <a href="#">
+                <a href="https://www.instagram.com/jeffreyy_30/"  target="_blank" rel="noopener noreferrer">
                     <span className={styles.iconInstagram}></span>
                 </a>
-                <a href="#">
+                <a href="https://api.whatsapp.com/send?phone=5561982242660"  target="_blank" rel="noopener noreferrer">
                     <span className={styles.iconWhatsapp}></span>
                 </a>
-                <a href="#">
+                <a href="https://www.linkedin.com/in/jefferson-c-silva-aa1b7b1a9/"  target="_blank" rel="noopener noreferrer">
                     <span className={styles.iconLinkedin}></span>
                 </a>
             </div>
