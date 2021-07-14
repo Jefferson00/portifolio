ba601983eb4262486385b8df6867fc8900639fff
Jefferson00 <jeffersonts00@gmail.com>
2021-07-14 17:37:46 -0300

    create fullscreen image view and lovepets web images added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,45 +4,45 @@ import { Projects } from '../components/Projects'
 import { ProjectsContext } from '../contexts/ProjectsContext';
 import { connectToDatabase } from '../utils/mongodb';
 
-interface TechnologiesData{
-  title:string,
-  class:string
+interface TechnologiesData {
+  title: string,
+  class: string
 }
 
-interface ProjectData{
-  id:string;
-  title:string;
-  description:string;
-  technologies:TechnologiesData[];
-  repository:string;
-  link:string;
-  thumbnail:string;
+interface ProjectData {
+  id: string;
+  title: string;
+  description: string;
+  technologies: TechnologiesData[];
+  repository: string;
+  link: string;
+  thumbnail: string;
 }
 
-interface HomeProps{
+interface HomeProps {
   projects: ProjectData[];
 }
 
-export default function Home({projects} :HomeProps) {
-  const {clickInProject} = useContext(ProjectsContext);
+export default function Home({ projects }: HomeProps) {
+  const { clickInProject } = useContext(ProjectsContext);
 
   return (
-      <>
-        {clickInProject ? (
-            <div style={{position:'fixed', display:'flex', top:'50%', left:'50%', transform: 'translate(-50%, -50%)', height:'100vh',width:'100vw', background:'#111518', zIndex:10}}>
-              <img src="/load.svg" alt="loading" width={"100%"} height={'100%'}/>
-            </div>
-        ): (
-          <>
-            <Projects projects={projects}/>
-          </>
-        )}
-      </>
+    <>
+      {clickInProject ? (
+        <div style={{ position: 'fixed', display: 'flex', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', height: '100vh', width: '100vw', background: '#111518', zIndex: 10 }}>
+          <img src="/load.svg" alt="loading" width={"100%"} height={'100%'} />
+        </div>
+      ) : (
+        <>
+          <Projects projects={projects} />
+        </>
+      )}
+    </>
   )
 }
 
-export const getStaticProps : GetStaticProps = async () => {
-  
+export const getStaticProps: GetStaticProps = async () => {
+
 
   const db = await connectToDatabase(process.env.MONGODB_URI);
 
@@ -50,13 +50,13 @@ export const getStaticProps : GetStaticProps = async () => {
   const projects = await db
     .collection('projects')
     .find({})
-    .sort({order:1})
+    .sort({ order: 1 })
     .toArray();
 
-  return{
-      props:{
-          projects: JSON.parse(JSON.stringify(projects))
-      },
-      revalidate:60*60*8,
+  return {
+    props: {
+      projects: JSON.parse(JSON.stringify(projects))
+    },
+    revalidate: 60,
   }
 }
--- src/pages/projects/[slug].tsx
+++ src/pages/projects/[slug].tsx
@@ -1,40 +1,43 @@
 import { GetStaticPaths, GetStaticProps } from "next"
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { ProjectsContext } from "../../contexts/ProjectsContext";
 import styles from '../../styles/project.module.css'
 import { connectToDatabase } from "../../utils/mongodb";
 
 
-interface TechnologiesData{
-    title:string,
-    class:string
+interface TechnologiesData {
+    title: string,
+    class: string
 }
 
-interface GalleryData{
-    image:string;
+interface GalleryData {
+    image: string;
 }
 
-interface Project{
-    id:string;
-    title:string;
-    description:string;
-    technologies:TechnologiesData[];
-    repository:string;
-    link:string;
-    thumbnail:string;
-    gallery:GalleryData[];
-    useCase:string;
+interface Project {
+    id: string;
+    title: string;
+    description: string;
+    technologies: TechnologiesData[];
+    repository: string;
+    link: string;
+    thumbnail: string;
+    gallery: GalleryData[];
+    useCase: string;
 }
 
-interface ProjectProps{
+interface ProjectProps {
     project: Project,
 }
 
 
-export default function Project({project}: ProjectProps){
-    const {updateButtonClicked, loadedProject} = useContext(ProjectsContext)
+export default function Project({ project }: ProjectProps) {
+    const { updateButtonClicked, loadedProject } = useContext(ProjectsContext)
+    const fullscreenContainerRef = useRef<HTMLDivElement>(null)
+    const [selectedImage, setSelectedImage] = useState('/projects/lovepets/mobile/preview-1.jpg')
+    const [fullScreenVisible, setFullScreenVisible] = useState(false)
 
-    function setIconClass(techClass:string){
+    function setIconClass(techClass: string) {
         switch (techClass) {
             case "iconReact":
                 return styles.iconReact
@@ -65,81 +68,113 @@ export default function Project({project}: ProjectProps){
         }
     }
 
-    useEffect(()=>{
+    const handleWheel = (event) => {
+        if (event.deltaY > 0) {
+            event.target.scrollBy(300, 0)
+        } else {
+            event.target.scrollBy(-300, 0)
+        }
+
+    }
+
+    const showFullscrenImage = (image: string) => {
+        setSelectedImage(image)
+
+        fullscreenContainerRef.current.style.visibility = 'visible'
+        fullscreenContainerRef.current.style.opacity = '1'
+    }
+
+    const hideFullscrenImage = () => {
+        setSelectedImage('')
+
+        fullscreenContainerRef.current.style.opacity = '0'
+        fullscreenContainerRef.current.style.visibility = 'hidden'
+    }
+
+    useEffect(() => {
         updateButtonClicked()
         loadedProject()
-    },[])
+    }, [])
 
-    return(
-        <div className={styles.projectContainer} id="project">
-            <div className={styles.detailsContainer}>
-                <div className={styles.descriptionContainer}>
-                    <p>{project.title}</p>
-                    <div className={styles.descriptionBox} dangerouslySetInnerHTML={{__html:project.description}}>
-                    </div>
-                </div>
-
-                <div className={styles.linksContainer}>
-                    <a href={project.link}  target="_blank" rel="noopener noreferrer">
-                        <span className={styles.iconExternal}></span>
-                    </a>
-                    <a href={project.repository}  target="_blank" rel="noopener noreferrer">
-                        <span className={styles.iconGithub}></span>
-                    </a>
-                </div>
-                
-            </div>
-
-            <div className={styles.thumbContainer}>
-                <img src={project.thumbnail} alt="thumbnail" />
-            </div>
-
-            <div className={styles.iconsContainer}>
-                {project.technologies.map((tech:TechnologiesData, index:number) =>{
-                    return(
-                        <span className={setIconClass(tech.class)} key={index}>
-                            <div className={styles.iconsLegend}>
-                                {tech.title}
-                            </div>
-                            <div className={styles.iconsLegendMobile}>
-                                <p>{tech.title}</p>
-                            </div>
-                        </span>
-                    )
-                })}
-            </div>
-
-            <div className={styles.galleryContainer}>
-                {project.gallery.map((image, index) =>{
-                    return(
-                        <div className={styles.galleryItem} key={index}>
-                            <img src={image.image} alt={`preview - ${index}`} />
+    return (
+        <>
+            <div className={styles.projectContainer} id="project">
+                <div className={styles.detailsContainer}>
+                    <div className={styles.descriptionContainer}>
+                        <p>{project.title}</p>
+                        <div className={styles.descriptionBox} dangerouslySetInnerHTML={{ __html: project.description }}>
                         </div>
-                    )
-                })} 
-            </div>
-
-
-            <div className={styles.descriptionContainer}>
-                    <p>Caso de estudo</p>
-                    <div className={styles.descriptionBox} dangerouslySetInnerHTML={{__html:project.useCase}}>
                     </div>
-                <div className={styles.linksContainer} style={{flexDirection:'row'}}>
-                        <a href={project.link}  target="_blank" rel="noopener noreferrer" style={{marginRight:16}}>
+
+                    <div className={styles.linksContainer}>
+                        <a href={project.link} target="_blank" rel="noopener noreferrer">
                             <span className={styles.iconExternal}></span>
                         </a>
-                        <a href={project.repository}  target="_blank" rel="noopener noreferrer">
+                        <a href={project.repository} target="_blank" rel="noopener noreferrer">
                             <span className={styles.iconGithub}></span>
                         </a>
+                    </div>
+
+                </div>
+
+                <div className={styles.thumbContainer}>
+                    <img src={project.thumbnail} alt="thumbnail" />
+                </div>
+
+                <div className={styles.iconsContainer}>
+                    {project.technologies.map((tech: TechnologiesData, index: number) => {
+                        return (
+                            <span className={setIconClass(tech.class)} key={index}>
+                                <div className={styles.iconsLegend}>
+                                    {tech.title}
+                                </div>
+                                <div className={styles.iconsLegendMobile}>
+                                    <p>{tech.title}</p>
+                                </div>
+                            </span>
+                        )
+                    })}
+                </div>
+
+                <div className={styles.galleryContainer} onWheel={e => handleWheel(e)}>
+                    {project.gallery.map((image, index) => {
+                        return (
+                            <div className={styles.galleryItem} key={index}>
+                                <img src={image.image} alt={`preview - ${index}`}
+                                    onClick={() => showFullscrenImage(image.image)} />
+                            </div>
+                        )
+                    })}
+                </div>
+
+
+                <div className={styles.descriptionContainer}>
+                    <p>Caso de estudo</p>
+                    <div className={styles.descriptionBox} dangerouslySetInnerHTML={{ __html: project.useCase }}>
+                    </div>
+                    <div className={styles.linksContainer} style={{ flexDirection: 'row' }}>
+                        <a href={project.link} target="_blank" rel="noopener noreferrer" style={{ marginRight: 16 }}>
+                            <span className={styles.iconExternal}></span>
+                        </a>
+                        <a href={project.repository} target="_blank" rel="noopener noreferrer">
+                            <span className={styles.iconGithub}></span>
+                        </a>
+                    </div>
                 </div>
             </div>
 
-
-        </div>
+            <div
+                ref={fullscreenContainerRef}
+                className={styles.fullScreenImage}
+                onClick={hideFullscrenImage}
+            >
+                <img src={selectedImage} alt='' />
+            </div>
+        </>
     )
 }
 
-export const getStaticPaths: GetStaticPaths = async () =>{
+export const getStaticPaths: GetStaticPaths = async () => {
     const db = await connectToDatabase(process.env.MONGODB_URI);
 
     const projects = await db
@@ -149,33 +184,33 @@ export const getStaticPaths: GetStaticPaths = async () =>{
         .toArray();
 
     const paths = projects.map(project => {
-        return{
-            params:{
-                slug:project.id
+        return {
+            params: {
+                slug: project.id
             }
         }
     })
 
-    return{
+    return {
         paths,
         fallback: 'blocking',
     }
 }
 
-export const getStaticProps : GetStaticProps = async (ctx) => {
+export const getStaticProps: GetStaticProps = async (ctx) => {
     const db = await connectToDatabase(process.env.MONGODB_URI);
-    const {slug} = ctx.params;
+    const { slug } = ctx.params;
 
 
     const projects = await db
-    .collection('projects')
-    .findOne({id:slug});
-    
+        .collection('projects')
+        .findOne({ id: slug });
 
-    return{
-        props:{
+
+    return {
+        props: {
             project: JSON.parse(JSON.stringify(projects))
         },
-        revalidate: 60*60*8, 
+        revalidate: 60 * 60 * 8,
     }
 }
