57413ebb3364a068dd680e5ddb0b9906ada849cb
Jefferson00 <jeffersonts00@gmail.com>
2021-09-13 11:05:11 -0300

    feat: add list projects animation

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,9 +1,14 @@
 import { useContext } from 'react';
-import { ProjectsContext } from '../contexts/ProjectsContext';
-import styles from '../styles/components/Projects.module.css';
 import Link from 'next/link';
+
+import { ProjectsContext } from '../contexts/ProjectsContext';
 import { setIconClass } from '../utils/setIconClass';
 
+import { InView } from "react-intersection-observer";
+import { motion } from "framer-motion";
+import { fadeInUp, fadeInLeft } from '../styles/animations';
+
+import styles from '../styles/components/Projects.module.css';
 
 interface TechnologiesData {
     title: string,
@@ -27,7 +32,7 @@ interface HomeProps {
 }
 
 export function Projects({ projects }: HomeProps) {
-    const { clickedInProject } = useContext(ProjectsContext)
+    const { clickedInProject } = useContext(ProjectsContext);
 
     return (
         <>
@@ -37,49 +42,72 @@ export function Projects({ projects }: HomeProps) {
                         <strong>PROJETOS</strong>
                     </button>
                 </div>
-                <div className={styles.projectsContainer}>
+                <motion.div
+
+                    className={styles.projectsContainer}
+                >
                     <div className={styles.projectsList}>
                         {projects.map((project: ProjectData, index: number) => {
                             return (
-                                <div className={styles.projectItem} key={index}>
-                                    <div className={styles.imageContainer}>
-                                        <Link href={`/projects/${project.id}#project`}>
-                                            <a>
-                                                <span onClick={clickedInProject}>
-                                                    <img src="/icons/eye.svg" alt="ver" />
-                                                </span>
-                                            </a>
-                                        </Link>
-                                        {project.thumbnail_small ?
-                                            <img src={project.thumbnail_small} alt={project.title} />
-                                            :
-                                            <img src={project.thumbnail} alt={project.title} />
-                                        }
-                                    </div>
-                                    <div className={styles.titleContainer}>
-                                        <p>{project.title}</p>
-                                    </div>
-                                    <div className={styles.descriptionContainer} dangerouslySetInnerHTML={{ __html: project.resume }}>
-                                    </div>
-                                    <div className={styles.iconsContainer}>
-                                        {project.technologies.map((tech: TechnologiesData, index: number) => {
-                                            return (
-                                                <span className={setIconClass(tech.class)} key={index}>
-                                                    <div className={styles.iconsLegend}>
-                                                        {tech.title}
-                                                    </div>
-                                                    <div className={styles.iconsLegendMobile}>
-                                                        <p>{tech.title}</p>
-                                                    </div>
-                                                </span>
-                                            )
-                                        })}
-                                    </div>
-                                </div>
+                                <InView key={index}>
+                                    {({ ref, inView }) => (
+                                        <motion.div
+                                            ref={ref}
+                                            variants={fadeInUp}
+                                            initial='initial'
+                                            animate={inView ? 'animate' : 'initial'}
+                                            className={styles.projectItem}
+
+                                        >
+                                            <motion.div
+                                                variants={fadeInLeft}
+                                                initial='initial'
+                                                animate={inView ? 'animate' : 'initial'}
+                                                className={styles.imageContainer}
+                                            >
+                                                <Link href={`/projects/${project.id}#project`}>
+                                                    <a>
+                                                        <span onClick={clickedInProject}>
+                                                            <img src="/icons/eye.svg" alt="ver" />
+                                                        </span>
+                                                    </a>
+                                                </Link>
+                                                {project.thumbnail_small ?
+                                                    <img src={project.thumbnail_small} alt={project.title} />
+                                                    :
+                                                    <img src={project.thumbnail} alt={project.title} />
+                                                }
+                                            </motion.div>
+                                            <div className={styles.titleContainer}>
+                                                <p>{project.title}</p>
+                                            </div>
+                                            <div
+                                                className={styles.descriptionContainer}
+                                                dangerouslySetInnerHTML={{ __html: project.resume }}
+                                            >
+                                            </div>
+                                            <div className={styles.iconsContainer}>
+                                                {project.technologies.map((tech: TechnologiesData, index: number) => {
+                                                    return (
+                                                        <span className={setIconClass(tech.class)} key={index}>
+                                                            <div className={styles.iconsLegend}>
+                                                                {tech.title}
+                                                            </div>
+                                                            <div className={styles.iconsLegendMobile}>
+                                                                <p>{tech.title}</p>
+                                                            </div>
+                                                        </span>
+                                                    )
+                                                })}
+                                            </div>
+                                        </motion.div>
+                                    )}
+
+                                </InView>
                             )
                         })}
                     </div>
-                </div>
+                </motion.div>
             </div>
         </>
     )
--- src/styles/animations/index.ts
+++ src/styles/animations/index.ts
@@ -9,9 +9,47 @@ export const fadeInUp = {
     y: 0,
     opacity: 1,
     transition: {
+      delay: 0.2,
       duration: 0.6,
-      delay: 1,
       ease: easing,
     }
   },
 }
+
+export const fadeInLeft = {
+  initial: {
+    x: -120,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      delay: 0.2,
+      duration: 0.6,
+      ease: easing,
+    }
+  },
+}
+
+export const stagger = {
+  initial: {
+    y: 60,
+    opacity: 0,
+    transition: {
+      duration: 0.6,
+      ease: easing,
+      when: "beforeChildren",
+      staggerChildren: 0.3,
+    },
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.6,
+      ease: easing,
+      when: "afterChildren",
+    }
+  },
+};
