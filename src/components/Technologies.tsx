53df72d7f023a4caa037b5724117b89ee3563c8c
Jefferson00 <jeffersonts00@gmail.com>
2022-02-18 17:00:11 -0300

    feat: animation

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -1,36 +1,62 @@
-import styles from '../styles/components/Technologies.module.css'
+import { useEffect, useRef } from "react";
+import styles from "../styles/components/Technologies.module.css";
 
-export function Technologies(){
+export function Technologies() {
+  const containerRef = useRef<HTMLDivElement>(null);
 
-    return(
-        <div className={`${styles.techContainer} ${styles.showUpAnimation}`}>
-             <div className={styles.splitUpReact}>
-                <span className={`${styles.iconReact} ${styles.movingReact}`}/> 
-             </div>
-             <div className={styles.splitUpNode}>
-                <span className={`${styles.iconNode} ${styles.movingNode}`}/>
-             </div>
-             <div className={styles.splitUpJavascript}>
-                <span className={`${styles.iconJavascript} ${styles.movingJavascript}`}/>
-             </div>
-             <div className={styles.splitUpTypescript}>
-                 <span className={`${styles.iconTypescript} ${styles.movingTypescript}`}/>
-             </div>
-             <div className={styles.splitUpCss}>
-                <span className={`${styles.iconCss} ${styles.movingCss}`}/>
-             </div>
-             <div className={styles.splitUpHtml}>
-                <span className={`${styles.iconHtml} ${styles.movingHtml}`}/>
-             </div>
-             <div className={styles.splitUpNext}>
-                <span className={`${styles.iconNext} ${styles.movingNext}`}/>
-             </div>
-            <div className={styles.splitUpSql}>
-                <span className={`${styles.iconSql} ${styles.movingSql}`}/>
-            </div>
-            <div className={styles.splitUpNest}>
-                <span className={`${styles.iconNest} ${styles.movingNest}`}/>
-            </div>
-        </div>
-    )
+  /* useEffect(() => {
+    const items = containerRef.current
+      ?.childNodes as NodeListOf<HTMLDivElement>;
+
+    setTimeout(() => {
+      items?.forEach((item) => {
+        item.classList.forEach((cls) => {
+          if (cls.includes("light")) {
+            console.log(Math.floor(Math.random() * 10));
+            item.style.top = `${Math.floor(Math.random() * 100)}%`;
+            item.style.animationDelay = `${Math.floor(Math.random() * 20)}s`;
+            item.style.animationDirection = `${Math.floor(
+              Math.random() * 10
+            )}s`;
+          }
+        });
+      });
+    }, 1000);
+  }); */
+
+  return (
+    <div className={styles.techContainer} ref={containerRef}>
+      <div>
+        <span className={`${styles.iconReact} ${styles.movingTypescript}`} />
+      </div>
+      <div>
+        <span className={`${styles.iconNode} ${styles.movingTypescript}`} />
+      </div>
+      <div>
+        <span
+          className={`${styles.iconJavascript} ${styles.movingTypescript}`}
+        />
+      </div>
+      <div>
+        <span
+          className={`${styles.iconTypescript} ${styles.movingTypescript}`}
+        />
+      </div>
+      <div className={styles.splitUpCss}>
+        <span className={`${styles.iconCss} ${styles.movingTypescript}`} />
+      </div>
+      <div className={styles.splitUpHtml}>
+        <span className={`${styles.iconHtml} ${styles.movingTypescript}`} />
+      </div>
+      <div className={styles.splitUpNext}>
+        <span className={`${styles.iconNext} ${styles.movingTypescript}`} />
+      </div>
+      <div className={styles.splitUpSql}>
+        <span className={`${styles.iconSql} ${styles.movingTypescript}`} />
+      </div>
+      <div className={styles.splitUpNest}>
+        <span className={`${styles.iconNest} ${styles.movingTypescript}`} />
+      </div>
+    </div>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,18 @@
-import { GetStaticProps } from 'next'
-import styles from '../styles/components/Background.module.css'
-import { useContext, useRef, useEffect, useState } from 'react'
-import { About } from '../components/About';
-import { Contact } from '../components/Contact';
-import { Header } from '../components/Header';
-import { Projects } from '../components/Projects'
-import { Socials } from '../components/Socials';
-import { ProjectsContext } from '../contexts/ProjectsContext';
-import { connectToDatabase } from '../utils/mongodb';
+import { GetStaticProps } from "next";
+import styles from "../styles/components/Background.module.css";
+import { useContext, useRef, useEffect, useState } from "react";
+import { About } from "../components/About";
+import { Contact } from "../components/Contact";
+import { Header } from "../components/Header";
+import { Projects } from "../components/Projects";
+import { Socials } from "../components/Socials";
+import { ProjectsContext } from "../contexts/ProjectsContext";
+import { connectToDatabase } from "../utils/mongodb";
+import { Technologies } from "../components/Technologies";
 
 interface TechnologiesData {
-  title: string,
-  class: string
+  title: string;
+  class: string;
 }
 
 interface ProjectData {
@@ -36,28 +37,40 @@ export default function Home({ projects }: HomeProps) {
   const [positionY, setPositionY] = useState<number>();
   const hiddenRef = useRef<HTMLDivElement>(null);
   const repeat = 5;
-  let timeout
+  let timeout;
 
-  useEffect(() => {
-    hiddenRef.current.addEventListener('mousemove', (event) => {
-      if(timeout) clearTimeout(timeout);
-      setPositionX(event.clientX + 751)
-      setPositionY(event.clientY + 379)
-    })
-  },[])
+  /* useEffect(() => {
+    hiddenRef.current.addEventListener("mousemove", (event) => {
+      if (timeout) clearTimeout(timeout);
+      setPositionX(event.clientX + 751);
+      setPositionY(event.clientY + 379);
+    });
+  }, []); */
 
   return (
     <>
       {clickInProject ? (
-        <div style={{ position: 'fixed', display: 'flex', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', height: '100vh', width: '100vw', background: '#111518', zIndex: 10 }}>
-          <img src="/load.svg" alt="loading" width={"100%"} height={'100%'} />
+        <div
+          style={{
+            position: "fixed",
+            display: "flex",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            height: "100vh",
+            width: "100vw",
+            background: "#111518",
+            zIndex: 10,
+          }}
+        >
+          <img src="/load.svg" alt="loading" width={"100%"} height={"100%"} />
         </div>
       ) : (
         <>
-        <div className="headerWrapper" ref={hiddenRef}>
-          <Header />
-          <Socials />
-          <div className={styles.hiddenContainer} ref={hiddenRef} />
+          <div className="headerWrapper" ref={hiddenRef}>
+            <Header />
+            <Socials />
+            {/* <div className={styles.hiddenContainer} ref={hiddenRef} />
           <div 
         className={styles.backgroundContainer} 
         style={{
@@ -66,33 +79,31 @@ export default function Home({ projects }: HomeProps) {
         }}  
       >
       
-    </div>
-        </div>
+    </div> */}
+            <Technologies />
+          </div>
           <Projects projects={projects} />
           <Contact />
           <About />
         </>
       )}
     </>
-  )
+  );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-
-
   const db = await connectToDatabase(process.env.MONGODB_URI);
 
-
   const projects = await db
-    .collection('projects')
-    .find({ "arquived": false })
+    .collection("projects")
+    .find({ arquived: false })
     .sort({ order: 1 })
     .toArray();
 
   return {
     props: {
-      projects: JSON.parse(JSON.stringify(projects))
+      projects: JSON.parse(JSON.stringify(projects)),
     },
     revalidate: 60,
-  }
-}
+  };
+};
