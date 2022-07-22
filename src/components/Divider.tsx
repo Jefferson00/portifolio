c69168f361da207ef6f7db85c80fa38a0e26ab83
Jefferson00 <jeffersonts00@gmail.com>
2022-07-22 15:48:22 -0300

    feat: add divider

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -3,22 +3,23 @@ import styles from "../styles/components/Divider.module.css";
 export default function Divider() {
   return (
     <div className={styles.divider}>
-      <span className={`${styles.code} ${styles.one}`} />
+      {/*    <span className={`${styles.code} ${styles.one}`} />
       <span className={`${styles.code} ${styles.two}`} />
       <span className={`${styles.code} ${styles.three}`} />
-      <span className={`${styles.code} ${styles.four}`} />
+      <span className={`${styles.code} ${styles.four}`} /> */}
+
       <svg
-        width="1119"
+        width="1229"
         height="1097"
-        viewBox="0 0 1119 1097"
+        viewBox="0 0 1229 1097"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
         <g id="person">
-          <g id="monitor">
+          <g id="monitor" className={styles.monitor}>
             <rect
               id="borda"
-              x="149"
+              x="169.999"
               width="864"
               height="528"
               rx="20"
@@ -26,7 +27,7 @@ export default function Divider() {
             />
             <rect
               id="tela"
-              x="175"
+              x="195.999"
               y="16"
               width="812"
               height="496"
@@ -34,83 +35,83 @@ export default function Divider() {
               fill="#362C2C"
             />
           </g>
-          <g id="head">
+          <g id="head" className={styles.head}>
             <path
               id="Ellipse 4"
-              d="M740.511 350.742C740.511 453.252 663.358 586.973 568.184 586.973C473.011 586.973 395.858 453.252 395.858 350.742C395.858 248.233 473.011 165.133 568.184 165.133C663.358 165.133 740.511 248.233 740.511 350.742Z"
+              d="M761.511 350.742C761.511 453.252 684.358 586.973 589.184 586.973C494.011 586.973 416.858 453.252 416.858 350.742C416.858 248.233 494.011 165.133 589.184 165.133C684.358 165.133 761.511 248.233 761.511 350.742Z"
               fill="#E7C9A5"
             />
             <path
               id="Ellipse 7"
-              d="M403.35 369.374C403.35 393.254 416.547 422.104 409.719 422.104C402.891 422.104 379 393.254 379 369.374C379 345.494 380.789 325.081 387.616 325.081C394.444 325.081 403.35 345.494 403.35 369.374Z"
+              d="M424.351 369.374C424.351 393.254 437.547 422.104 430.719 422.104C423.891 422.104 400 393.254 400 369.374C400 345.494 401.789 325.081 408.616 325.081C415.444 325.081 424.351 345.494 424.351 369.374Z"
               fill="#EBB778"
             />
             <path
               id="Ellipse 8"
-              d="M725.319 375.044C725.319 400.655 711.402 431.595 718.603 431.595C725.803 431.595 751 400.655 751 375.044C751 349.434 749.114 327.542 741.913 327.542C734.712 327.542 725.319 349.434 725.319 375.044Z"
+              d="M746.319 375.044C746.319 400.655 732.402 431.595 739.603 431.595C746.803 431.595 772 400.655 772 375.044C772 349.434 770.114 327.542 762.913 327.542C755.712 327.542 746.319 349.434 746.319 375.044Z"
               fill="#EBB471"
             />
             <path
               id="Ellipse 6"
-              d="M744.257 333.33C701.414 363.586 790.106 492 567.623 565.178C363.554 519.089 418.8 326.645 390.613 326.645C390.613 227.815 475.675 130.683 567.623 130.683C659.571 130.683 744.257 234.499 744.257 333.33Z"
+              d="M765.257 333.33C722.414 363.586 811.106 492 588.623 565.178C384.555 519.089 439.8 326.645 411.613 326.645C411.613 227.815 496.675 130.683 588.623 130.683C680.571 130.683 765.257 234.499 765.257 333.33Z"
               fill="url(#paint0_linear_1043_484)"
             />
             <path
               id="Ellipse 9"
-              d="M629.997 128.81C622.504 176.761 606.18 211.601 576.801 211.601C547.421 211.601 530.347 159.903 523.604 128.81C520.233 77.4864 536.341 64 576.8 64C628.124 64 637.489 80.858 629.997 128.81Z"
+              d="M650.997 128.81C643.504 176.761 627.18 211.601 597.801 211.601C568.421 211.601 551.348 159.903 544.604 128.81C541.233 77.4864 557.341 64 597.801 64C649.124 64 658.489 80.858 650.997 128.81Z"
               fill="url(#paint1_linear_1043_484)"
             />
             <path
               id="Rectangle 15"
-              d="M551 198.698V198.698C564.058 218.318 593.492 217.567 605 197V197V197C590.352 211.777 566.603 212.463 551 198.698V198.698Z"
+              d="M572 198.698V198.698C585.058 218.318 614.492 217.567 626 197V197V197C611.352 211.777 587.603 212.463 572 198.698V198.698Z"
               fill="#D9D9D9"
             />
           </g>
           <g id="capuz">
             <path
               id="Ellipse 5"
-              d="M929.627 568.509C1104.17 652.497 822.694 707.849 590.605 729C337.16 729 34.8397 635.396 253.022 568.509C247.974 516.522 274.5 493.5 324.5 466.5C352 408 448.328 392.5 590.605 392.5C654 392.5 911.405 415.786 929.627 568.509Z"
+              d="M950.571 579.493C1122.09 668.722 845.495 727.529 617.438 750C368.395 750 71.3256 650.554 285.718 579.493C280.758 524.262 306.823 499.803 355.955 471.118C382.977 408.967 477.632 392.5 617.438 392.5C679.731 392.5 932.666 417.239 950.571 579.493Z"
               fill="url(#paint2_linear_1043_484)"
             />
             <path
               id="Rectangle 16"
-              d="M305.5 597C340.5 613.5 377 623 377 623L318.686 611.631C304.81 608.925 291.899 602.591 281.267 593.273L253 568.5C253 568.5 270.5 580.5 305.5 597Z"
+              d="M337.285 609.762C371.677 627.292 407.543 637.385 407.543 637.385L351.541 625.58C337.079 622.532 323.745 615.537 313.016 605.371L285.697 579.484C285.697 579.484 302.893 592.233 337.285 609.762Z"
               fill="#262E35"
             />
             <path
               id="Rectangle 17"
-              d="M861.825 597.625C898.668 585.802 931.831 567.838 931.831 567.838L881.256 598.231C870.567 604.655 858.524 608.49 846.089 609.432L804 612.619C804 612.619 824.981 609.449 861.825 597.625Z"
+              d="M883.946 610.426C920.15 597.865 952.737 578.78 952.737 578.78L902.767 611.247C892.444 617.955 880.822 621.96 868.835 622.941L827.126 626.356C827.126 626.356 847.742 622.988 883.946 610.426Z"
               fill="#262E35"
             />
             <path
               id="Rectangle 18"
-              d="M358.5 556.001C393.861 568.766 520 594.718 574.5 591.938C612.5 590 702 594.438 786.5 564C812.063 554.792 828.772 468.5 857 490.938C951.792 566.285 704.5 600.5 574.5 604C444.5 607.5 219.059 560.265 289.5 506.876C329 476.938 300.5 535.063 358.5 556.001Z"
+              d="M389.364 566.204C424.111 579.766 548.059 607.338 601.612 604.384C638.952 602.325 726.897 607.04 809.93 574.703C835.049 564.921 851.468 473.243 879.206 497.081C972.35 577.131 729.354 613.481 601.612 617.199C473.87 620.917 252.345 570.735 321.563 514.014C360.376 482.208 332.371 543.96 389.364 566.204Z"
               fill="#262E35"
             />
           </g>
           <g id="corpo">
             <path
-              id="Union"
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M941.883 920.592L915.5 1097H251.5L231.369 887.259L124.076 977.474C94.7186 1002.16 51.7857 1002.38 22.0001 978.216C-2.79541 954.161 -6.50974 915.683 13.2267 887.329L192 630.5L213.531 649H924.101L967.81 616.711L1110.11 893.813C1127.64 927.933 1118.29 969.837 1088.09 993.488C1059.12 1012.3 1020.78 1007.35 997.534 981.788L941.883 920.592Z"
+              id="arm-1"
+              d="M227.733 637L398.233 783.5L159.809 983.974C130.451 1008.66 87.5185 1008.88 57.7329 984.716V984.716V984.716C32.9374 960.661 29.2231 922.183 48.9595 893.829L227.733 637Z"
+              fill="black"
+              className={styles.armOne}
+            />
+            <path
+              id="arm-2"
+              d="M808 750.279L988.81 616.711L1131.11 893.813C1148.64 927.933 1139.29 969.837 1109.09 993.488V993.488V993.488C1080.12 1012.3 1041.78 1007.35 1018.53 981.788L808 750.279Z"
+              fill="black"
+              className={styles.armTwo}
+            />
+            <path
+              id="Rectangle 12"
+              d="M229.499 649H1003.5L936.499 1097H272.499L229.499 649Z"
               fill="url(#paint3_linear_1043_484)"
-            />
-            <path
-              id="Rectangle 20"
-              d="M943.07 911.774C943.07 911.774 946.151 894.764 939.864 896.209C936.092 897.076 937.071 901.597 937.071 901.597L937.652 906.103L940.635 928.141L943.07 911.774Z"
-              fill="#262E35"
-            />
-            <path
-              id="Rectangle 19"
-              d="M230.5 879C230.5 879 228.5 861.833 233.5 863.5C236.5 864.5 235.591 869 235.591 869L235 873.5L232 895.5L230.5 879Z"
-              fill="#262E35"
             />
           </g>
           <g id="linhas-2">
             <rect
               id="linha-36"
-              x="717"
+              x="738"
               y="48"
               width="238"
               height="12"
@@ -119,7 +120,7 @@ export default function Divider() {
             />
             <rect
               id="linha-39"
-              x="878"
+              x="899"
               y="102"
               width="77"
               height="12"
@@ -128,7 +129,7 @@ export default function Divider() {
             />
             <rect
               id="linha-40"
-              x="751"
+              x="772"
               y="102"
               width="108"
               height="12"
@@ -137,7 +138,7 @@ export default function Divider() {
             />
             <rect
               id="linha-41"
-              x="796"
+              x="817"
               y="129"
               width="159"
               height="12"
@@ -146,7 +147,7 @@ export default function Divider() {
             />
             <rect
               id="linha-42"
-              x="699"
+              x="720"
               y="156"
               width="258"
               height="12"
@@ -155,7 +156,7 @@ export default function Divider() {
             />
             <rect
               id="linha-43"
-              x="828"
+              x="849"
               y="183"
               width="127"
               height="12"
@@ -164,7 +165,7 @@ export default function Divider() {
             />
             <rect
               id="linha-44"
-              x="878"
+              x="899"
               y="210"
               width="77"
               height="12"
@@ -173,7 +174,7 @@ export default function Divider() {
             />
             <rect
               id="linha-46"
-              x="772"
+              x="793"
               y="237"
               width="185"
               height="12"
@@ -182,7 +183,7 @@ export default function Divider() {
             />
             <rect
               id="linha-47"
-              x="828"
+              x="849"
               y="264"
               width="132"
               height="12"
@@ -191,7 +192,7 @@ export default function Divider() {
             />
             <rect
               id="linha-48"
-              x="878"
+              x="899"
               y="291"
               width="82"
               height="12"
@@ -200,7 +201,7 @@ export default function Divider() {
             />
             <rect
               id="linha-50"
-              x="796"
+              x="817"
               y="319"
               width="164"
               height="12"
@@ -209,7 +210,7 @@ export default function Divider() {
             />
             <rect
               id="linha-49"
-              x="772"
+              x="793"
               y="291"
               width="96"
               height="12"
@@ -218,7 +219,7 @@ export default function Divider() {
             />
             <rect
               id="linha-45"
-              x="751"
+              x="772"
               y="210"
               width="108"
               height="12"
@@ -227,7 +228,7 @@ export default function Divider() {
             />
             <rect
               id="linha-38"
-              x="657"
+              x="678"
               y="75"
               width="157"
               height="12"
@@ -236,7 +237,7 @@ export default function Divider() {
             />
             <rect
               id="linha-37"
-              x="828"
+              x="849"
               y="75"
               width="127"
               height="12"
@@ -247,7 +248,7 @@ export default function Divider() {
           <g id="linhas-1">
             <rect
               id="linha-21"
-              x="203"
+              x="223.999"
               y="48"
               width="238"
               height="12"
@@ -256,7 +257,7 @@ export default function Divider() {
             />
             <rect
               id="linha-22"
-              x="203"
+              x="223.999"
               y="75"
               width="215"
               height="12"
@@ -265,7 +266,7 @@ export default function Divider() {
             />
             <rect
               id="linha-23"
-              x="203"
+              x="223.999"
               y="102"
               width="188"
               height="12"
@@ -274,7 +275,7 @@ export default function Divider() {
             />
             <rect
               id="linha-24"
-              x="203"
+              x="223.999"
               y="129"
               width="57"
               height="12"
@@ -283,7 +284,7 @@ export default function Divider() {
             />
             <rect
               id="linha-31"
-              x="426"
+              x="446.999"
               y="75"
               width="57"
               height="12"
@@ -292,7 +293,7 @@ export default function Divider() {
             />
             <rect
               id="linha-30"
-              x="266"
+              x="286.999"
               y="129"
               width="163"
               height="12"
@@ -301,7 +302,7 @@ export default function Divider() {
             />
             <rect
               id="linha-25"
-              x="203"
+              x="223.999"
               y="156"
               width="209"
               height="12"
@@ -310,7 +311,7 @@ export default function Divider() {
             />
             <rect
               id="linha-26"
-              x="203"
+              x="223.999"
               y="183"
               width="220"
               height="12"
@@ -319,7 +320,7 @@ export default function Divider() {
             />
             <rect
               id="linha-32"
-              x="203"
+              x="223.999"
               y="264"
               width="147"
               height="12"
@@ -328,7 +329,7 @@ export default function Divider() {
             />
             <rect
               id="linha-27"
-              x="203"
+              x="223.999"
               y="210"
               width="120"
               height="12"
@@ -337,7 +338,7 @@ export default function Divider() {
             />
             <rect
               id="linha-33"
-              x="203"
+              x="223.999"
               y="291"
               width="120"
               height="12"
@@ -346,7 +347,7 @@ export default function Divider() {
             />
             <rect
               id="linha-28"
-              x="203"
+              x="223.999"
               y="237"
               width="104"
               height="12"
@@ -355,7 +356,7 @@ export default function Divider() {
             />
             <rect
               id="linha-34"
-              x="203"
+              x="223.999"
               y="318"
               width="104"
               height="12"
@@ -364,7 +365,7 @@ export default function Divider() {
             />
             <rect
               id="linha-29"
-              x="318"
+              x="338.999"
               y="237"
               width="57"
               height="12"
@@ -373,7 +374,7 @@ export default function Divider() {
             />
             <rect
               id="linha-35"
-              x="318"
+              x="338.999"
               y="318"
               width="57"
               height="12"
@@ -385,9 +386,9 @@ export default function Divider() {
         <defs>
           <linearGradient
             id="paint0_linear_1043_484"
-            x1="567"
+            x1="588"
             y1="86"
-            x2="567"
+            x2="588"
             y2="255"
             gradientUnits="userSpaceOnUse"
           >
@@ -396,9 +397,9 @@ export default function Divider() {
           </linearGradient>
           <linearGradient
             id="paint1_linear_1043_484"
-            x1="578"
+            x1="599"
             y1="35.5"
-            x2="578"
+            x2="599"
             y2="174.5"
             gradientUnits="userSpaceOnUse"
           >
@@ -407,10 +408,10 @@ export default function Divider() {
           </linearGradient>
           <linearGradient
             id="paint2_linear_1043_484"
-            x1="581"
-            y1="367"
-            x2="581"
-            y2="700.5"
+            x1="607.999"
+            y1="393"
+            x2="607.999"
+            y2="617.5"
             gradientUnits="userSpaceOnUse"
           >
             <stop stop-color="#111518" />
@@ -418,14 +419,14 @@ export default function Divider() {
           </linearGradient>
           <linearGradient
             id="paint3_linear_1043_484"
-            x1="612"
-            y1="1097"
-            x2="604"
-            y2="655"
+            x1="616.999"
+            y1="784"
+            x2="616.999"
+            y2="1191.5"
             gradientUnits="userSpaceOnUse"
           >
-            <stop stop-color="#111518" />
-            <stop offset="1" />
+            <stop />
+            <stop offset="1" stop-color="#111518" />
           </linearGradient>
         </defs>
       </svg>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -66,7 +66,7 @@ export default function Home({ projects, secondaryProjects }: HomeProps) {
           <OthersProjects projects={secondaryProjects} />
         </>
       )}
-      <Divider />
+
       <Contact />
       <About />
     </>
